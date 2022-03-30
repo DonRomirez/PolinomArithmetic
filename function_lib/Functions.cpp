d3a60a1e1031b35fba57b2c5d7b3933658d31632
DonRomirez <romk.plastinin@mail.ru>
2022-03-30 23:35:36 +0300

    almost ready

--- function_lib/Functions.cpp
+++ function_lib/Functions.cpp
@@ -25,55 +25,6 @@ List<string> LexicalAnalisis(string str) {
 	}
 	return ListStrMonom;
 }
-List<MonomValue> PolinomList(List<string> ListStrMonom) {
-	string t = "";
-	Iterator<string> itr(ListStrMonom.GetFirst());
-	List<MonomValue> listPolinom;
-	while (itr.hasNext()) {
-		t = itr.next();
-		unsigned int pow[N] = { 0 };
-		double a = 1.0;
-		string t2 = "";
-		int ipow = -1;
-		for (int i = 0; i < t.size(); i++) {
-			if ((t[i] == 'x') || (t[i] == 'y') || (t[i] == 'z'))
-			{
-				if (ipow == -1)
-				{
-					if (t2.size() > 0)
-					{
-						a = atof(t2.c_str());
-						t2 = "";
-					}
-					if (t[i] == 'x') ipow = 0;
-					if (t[i] == 'y') ipow = 1;
-					if (t[i] == 'z') ipow = 2;
-					i++;
-				}
-				else
-				{
-					pow[ipow] = atoi(t2.c_str());
-					t2 = "";
-					if (t[i] == 'x') ipow = 0;
-					if (t[i] == 'y') ipow = 1;
-					if (t[i] == 'z') ipow = 2;
-					i++;
-				}
-			}
-			else
-			{
-				t2 += t[i];
-			}
-		}
-		if ((ipow >= 0) && (t2.size() > 0)) {
-			pow[ipow] = atoi(t2.c_str());
-		}
-		if ((ipow < 0) && (t2.size() > 0))
-			a = atoi(t2.c_str());
-		listPolinom.addlast(MonomValue(a, pow[0], pow[1], pow[2]));
-	}
-	return listPolinom;
-}
 Polynom AddPolynom(List<string> ListStrMonom){
 	Polynom p;
 	string t = "";
